a5d0d3cbbd10dc9f019cecadffc88e7d888a2f12
Rakhmanbedi <102344578+Rakhmanbedi@users.noreply.github.com>
2023-02-12 21:59:59 +0600

    pob

--- men/models.py
+++ men/models.py
@@ -8,3 +8,27 @@ class Men(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now_add=True)
     is_published = models.BooleanField(default=True)
+
+class Post(models.Model):
+     discription = models.TextField(blank=True)
+     price = models.IntegerField()
+     size = models.CharField(max_length=255)
+     image = models.ImageField(upload_to="photos/%Y/%m/%d/")
+     time_update = models.DateTimeField(auto_now_add=True)
+     is_published = models.BooleanField(default=True)
+
+class Users(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="photos/%Y/%m/%d/")
+
+class comments(models.Model):
+    comment = models.TextField(blank=True)
+
+class Categories(models.Model):
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=50)
+
+class Roles(models.Model):
+    name = models.CharField(max_length=20)
